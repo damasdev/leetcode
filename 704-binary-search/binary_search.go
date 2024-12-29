d1766757f55ab033d1023537129ebab1584fe6f8
Damas Amirul Karim <damasamirulkarim@gmail.com>
2024-12-29 20:55:52 +0700

    change to unit test

--- 704-binary-search/binary_search.go
+++ 704-binary-search/binary_search.go
@@ -1,11 +1,9 @@
-package main
-
-import "fmt"
+package search
 
 // Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
 // If target exists, then return its index. Otherwise, return -1.
 // You must write an algorithm with O(log n) runtime complexity.
-func search(nums []int, target int) int {
+func Search(nums []int, target int) int {
 	half := len(nums) / 2
 	if nums[half] == target {
 		return half
@@ -26,12 +24,4 @@ func search(nums []int, target int) int {
 	}
 
 	return -1
-}
-
-func main() {
-	nums := []int{-1, 0, 3, 5, 9, 12}
-	target := 9
-
-	val := search(nums, target)
-	fmt.Println(val)
 }
